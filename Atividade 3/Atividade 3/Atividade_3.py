b7dd0f41fc53ddb5ef744e1ed1d989607852f9c3
Carol Costa <carolcost161@gmail.com>
2024-05-20 15:00:47 -0300

    Atividade 3

---
+++ Atividade 3/Atividade 3/Atividade_3.py
@@ -0,0 +1,11 @@
+
+
+#resposta com for
+for numeroAndar in range (20,0,-1):
+	if numeroAndar != 13: print(numeroAndar)
+
+#resposta com while
+	numAndar = 20
+while numAndar < 0:
+    if numAndar != 13: print(numAndar)
+    numAndar -=1
