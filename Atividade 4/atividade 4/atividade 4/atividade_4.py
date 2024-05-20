58a16df5c4143cbfb24fbcf1778fc4aabba9e0bc
Carol Costa <carolcost161@gmail.com>
2024-05-20 15:47:06 -0300

    Atividade 4

---
+++ Atividade 4/atividade 4/atividade 4/atividade_4.py
@@ -0,0 +1,12 @@
+
+def calculaOperacao(num1, num2, operador):
+    if operador == 1:
+       return num1 + num2
+    elif operador == 2:
+       return num1 - num2
+    elif operador == 3:
+        return num1 * num2
+    elif operador == 4:
+        return num1 / num2
+    else:
+        return 0
